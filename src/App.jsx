6a41fc5623522aea1f59240a7171d660d841688d
Nilesh Parmar <nilesh2033parmar@gmail.com>
2026-04-17 15:36:45 +0530

    v1

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="container">
+      <div className="card">
+        <h1>🚀 DevOps Deployment Project</h1>
+        <p>This app is successfully deployed using CI/CD Pipeline</p>
+
+        <div className="box">
+          <h3>Tech Stack:</h3>
+          <ul>
+            <li>⚛️ React</li>
+            <li>📦 Node.js</li>
+            <li>🐙 GitHub</li>
+            <li>⚙️ GitHub Actions</li>
+            <li>🐧 Linux Server</li>
+          </ul>
+        </div>
+
+        <button className="btn">Deployment Successful ✅</button>
+      </div>
+    </div>
+  );
+}
